54f5b74e51abfddb8cfcb03e116928f04a92386d
sitmalidis <sitmalidi@yahoo.com>
2020-01-12 17:15:28 +0200

    13

--- src/main/java/Controllers/EmailController.java
+++ src/main/java/Controllers/EmailController.java
@@ -29,7 +29,7 @@ public class EmailController {
 
         Properties prop = new Properties();
         prop.put("mail.smtp.host", "smtp.gmail.com");
-        prop.put("mail.smtp.port", "25");
+        prop.put("mail.smtp.port", "587");
         prop.put("mail.smtp.auth", "true");
         prop.put("mail.smtp.starttls.enable", "true"); //TLS
 
